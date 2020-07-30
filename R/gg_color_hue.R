380531a90fd2dbc09e4d5da519cc078e96145619
altintasali <altintas.ali@gmail.com>
2020-07-30 11:09:12 +0200

    gg_color_hue

---
+++ R/gg_color_hue.R
@@ -0,0 +1,19 @@
+#'Generate ggplot2 default colors
+#'
+#'@param x Number of colors
+#'@return Vector with color hexcodes
+#'@examples
+#'gg_color_hue(2)
+#'# [1] "#F8766D" "#00BFC4"
+#'gg_color_hue(5)
+#'# [1] "#F8766D" "#A3A500" "#00BF7D" "#00B0F6" "#E76BF3"
+#'res <- uniqueNames(x, sep = "_")
+#'print(res)
+#'res2 <- uniqueNames(x, sep = ".")
+#'print(res2)
+#'@export
+
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
