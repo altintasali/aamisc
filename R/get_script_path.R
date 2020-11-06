ca2e9f7cbbc6028dc2245a78bdb8d3aa50b52c17
altintasali <altintas.ali@gmail.com>
2020-11-06 10:53:56 +0100

    get_script_path.R

---
+++ R/get_script_path.R
@@ -0,0 +1,35 @@
+#' Get current script's path
+#' @import rstudioapi
+#' @param keep_name Keep name of the script. Default is TRUE.
+#'
+#' @return Path of the current script
+#' @export
+#'
+#' @examples
+#' @references
+#' This script was inspired by \href{https://stackoverflow.com/a/49197577/1488825}{StackOverflow post} by user \href{https://stackoverflow.com/users/9131220/tinker}{Tinker}.
+#' Only added a parameter called "keep_name"
+
+get_script_path <- function(keep_name = TRUE) {
+  args <- commandArgs(trailingOnly = FALSE)
+  file <- "--file="
+  rstudio <- "RStudio"
+
+  if(keep_name){
+    pathFunction <- I
+  }else{
+    pathFunction <- dirname
+  }
+
+  match <- grep(rstudio, args)
+  if (length(match) > 0) {
+    return(pathFunction(rstudioapi::getSourceEditorContext()$path))
+  } else {
+    match <- grep(file, args)
+    if (length(match) > 0) {
+      return(pathFunction(normalizePath(sub(file, "", args[match]))))
+    } else {
+      return(pathFunction(normalizePath(sys.frames()[[1]]$ofile)))
+    }
+  }
+}
