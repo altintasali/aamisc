b8dc98f32a4ffd33145828837214ef76858f2f54
Ali Altintas <altintas.ali@gmail.com>
2025-07-02 11:54:28 +0200

    New R package documentation style implemented

---
+++ R/aamisc-package.R
@@ -0,0 +1,9 @@
+#' aamisc: Miscellaneous R Functions by Ali Altintas
+#'
+#' A collection of utility functions
+#'
+#' @author Ali Altintas \email{altintas.ali@gmail.com}
+#'
+#' @keywords internal
+"_PACKAGE"
+
